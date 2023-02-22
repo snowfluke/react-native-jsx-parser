7167914e703d6d402dc5414d022c3153aa8c1ce8
Fernando Stefanello <fernandostefa@gmail.com>
2023-02-22 10:53:20 -0300

    fix: remaining props

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import JsxParser from 'react-jsx-parser';
 
-const RNJsxParser = ({jsx, bindings, components}) => {
+const RNJsxParser = ({jsx, ...props}) => {
   let content = jsx.replace(/\n/g, ' ')
-      content = content.replace(/\t/g, ' ')
-      content = content.replace(/\s\s+/g, ' ')
-      content = content.replace(/> </g, '><')
+  content = content.replace(/\t/g, ' ')
+  content = content.replace(/\s\s+/g, ' ')
+  content = content.replace(/> </g, '><')
 
   return (
     <>
-      <JsxParser bindings={bindings} components={components} renderInWrapper= {false} jsx={content}/>
+      <JsxParser renderInWrapper={false} jsx={content} {...props} />
     </>
   )
 }
